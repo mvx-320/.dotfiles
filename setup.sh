be7ad031341bc7f97e2386327a5ce43f40b97186
balu <max0tremel+github@gmail.com>
2025-05-13 15:42:59 +0200

    Fixed Capslock override to Ctrl

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf ~/.dotfiles/.xprofile ~/.xprofile
