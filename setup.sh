b8d800882b94001be49bb37b57522d9fdd3675ec
balu <max0tremel+github@gmail.com>
2025-05-13 15:02:17 +0200

    ReAdd setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Creates links for the ~ directory
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.xprofile ~/.xprofile
